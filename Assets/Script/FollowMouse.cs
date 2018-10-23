4352891b0dbf1280f95ec208e233835c9cef63b7
TheGmas <44088293+TheGmas@users.noreply.github.com>
2018-10-23 23:43:57 +0100

    Initial commit

---
+++ Assets/Script/FollowMouse.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour {
+
+    private int check;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        check = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mouse = Input.mousePosition;
+        if (check == 1)
+        {
+
+            if (1 == 1)
+            {
+                mouse.z = 15f; // distance from camera
+            }
+
+            this.transform.position = Camera.main.ScreenToWorldPoint(mouse);
+
+            if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
+            {
+                mouse.z = mouse.z - 1f;
+            }
+
+            else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backwards
+            {
+                mouse.z--;
+            }
+
+            else if (Input.GetMouseButtonDown(0))
+            {
+                check = 2;
+            }
+        }
+    }
+}
+

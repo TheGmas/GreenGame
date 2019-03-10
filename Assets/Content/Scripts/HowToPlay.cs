e545462795459c681fff9f39e7ba1099ac958bae
Natalie Justice <n.justice@btinternet.com>
2019-03-10 12:45:20 +0000

    Adding how to play

---
+++ Assets/Content/Scripts/HowToPlay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HowToPlay : MonoBehaviour {
+
+    public GameObject HowToPlayPanel;
+    public Button playBtn;
+
+
+	// Use this for initialization
+	void Start () {
+        HowToPlayPanel.SetActive(true);
+        playBtn.onClick.AddListener(closePanel);
+	}
+	
+	// Update is called once per frame
+	void closePanel () {
+        HowToPlayPanel.SetActive(false);
+	}
+}
